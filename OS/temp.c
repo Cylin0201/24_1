b5c9b114ac1b2373da7272e04516a3ca9908359a
cylin0201 <cylin0201@naver.com>
2024-04-26 15:49:30 +0900

    hi

--- OS/temp.c
+++ OS/temp.c
@@ -4,10 +4,10 @@
 int main(){
     int pid;
 
-    if ((pid = fork()) == 0){  //자식 프로세스
+    if ((pid = fork()) == 0){   //자식 프로세스
         printf("Child PROCESS of %d is %d\n", getppid(), getpid());
     }
-    else{   //부모 프로세스
+    else{                       //부모 프로세스
         printf("I am %d. My child is %d\n", getpid(), pid);
     }
     return 0;

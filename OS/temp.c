a7ee8c7241eaa33f17cd3483a7b0d4a44d62899b
cylin0201 <cylin0201@naver.com>
2024-04-27 19:49:51 +0900

    add pthread exemple

--- OS/temp.c
+++ OS/temp.c
@@ -1,14 +1,20 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int pid;
+    int a, b;
+    if ((pid = fork()) == 0){   //child process returns 0.
+        int pid1 = getpid();
+        printf("child: pid = %d\n", pid);   //pid() child's return values => 0
+        printf("child: pid1 = %d\n", pid1); //child's pid1 => 7192
+    }
+    else{                       //parent process returns non-zero
+        int pid1 = getpid();
+        printf("parent: pid = %d\n", pid);  //pid() parent's returnn values => 7192
+        printf("parent: pid1 = %d\n", pid1);    //parent's pid1 => 7187
+    }
 
-    if ((pid = fork()) == 0){   //자식 프로세스
-        printf("Child PROCESS of %d is %d\n", getppid(), getpid());
-    }
-    else{                       //부모 프로세스
-        printf("I am %d. My child is %d\n", getpid(), pid);
-    }
     return 0;
 }
--- OS/temp2.c
+++ OS/temp2.c
@@ -1,17 +1,26 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <wait.h>
 
-int main(){
-    while(1){
-        char* cmd = read_command();
-        int pid;
-        if ((pid = fork()) == 0){
-            exec(cmd);
-            panic("exec failed!");
-        }
-        else wait(pid);
-    }
-
+int value = 0;
+void *runner(void *param){    /* the thread */
+    value = 5;
+    pthread_exit(0);
 }
+
+int main(){
+    pid_t        pid;
+    pthread_t    tid;
+    if ((pid = fork()) == 0)  {
+        pthread_create(&tid, NULL, runner, NULL);
+        pthread_join(tid, NULL);
+        printf("CHILD: value = %d\n", value);    /* LINE C */
+    }
+    else  {
+        wait(NULL);
+        printf("PARENT: value = %d\n", value);    /* LINE P */
+    }
+}            
